59bf28d99155094dd405477c516de00cc3c90b64
ayush <9956624639a@gmail.com>
2022-07-07 01:51:33 +0530

    second commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session=require('express-session')
 const ejsMate = require('ejs-mate')
 const flash = require('connect-flash');
 const UserSchema = require('./schema/UserSchema');
-
+const methodOverride = require('method-override');
 const signup= require('./routes/signup');
 const login = require('./routes/login');
 const passport = require('passport');
@@ -17,7 +17,7 @@ const LocalStrategy = require('passport-local');
 
 
  
-
+//connecting database
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fake-data', {
@@ -33,6 +33,7 @@ db.once("open", () => {
 });
 
 
+//session
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -44,22 +45,28 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+
+
+//passport middlewares
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(UserSchema.authenticate()));
 
+
 passport.serializeUser(UserSchema.serializeUser());
 passport.deserializeUser(UserSchema.deserializeUser());
 
+//other middlewares
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
-
-
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use((req, res, next) => {
@@ -83,6 +90,7 @@ app.use('/login', login);
 
 
 
+//listen
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
